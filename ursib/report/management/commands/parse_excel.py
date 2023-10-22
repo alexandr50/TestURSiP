f02ff2b58d46419a2c1cbd0fd5c9044e6c600570
alexandr <dmitrymaslov2016@mai.ru>
2023-10-22 18:16:30 +0300

    add docstrings

--- ursib/report/management/commands/parse_excel.py
+++ ursib/report/management/commands/parse_excel.py
@@ -6,11 +6,14 @@ from report.servces.seve_to_db import save_report, update_report
 
 
 class Command(BaseCommand):
-    help = "Parse excel file"
 
     def add_arguments(self, parser):
-        parser.add_argument("--path", type=str, default=r'report/data_files//Приложение_к_заданию_бек_разработчика.xlsx',
-                            help='Put the path from content root')
+        """Метод добавления пути к файлу"""
+
+        parser.add_argument("--path",
+                            type=str,
+                            default=r'report/data_files//Приложение_к_заданию_бек_разработчика.xlsx',
+                            )
 
     def handle(self, *args, **options):
         file_path = options.get('path')
@@ -20,4 +23,4 @@ class Command(BaseCommand):
             update_report
         )
         report.create_report(file_path)
-        print('success!')
+
