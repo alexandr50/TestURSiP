25a54d2648e4799aa66e99b960d1fae3bd38d1b9
alexandr <dmitrymaslov2016@mai.ru>
2023-10-22 18:24:02 +0300

    add env.sample

--- ursib/report/management/commands/parse_excel.py
+++ ursib/report/management/commands/parse_excel.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 from report.servces.parse_table import parse_table
 from report.servces.report_handler import ReportHandler
